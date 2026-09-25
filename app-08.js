e1142982abe971cf7d4fb1e72800d20874a10607
sam4ever1988 <gfhjr9z4dw@privaterelay.appleid.com>
2026-09-25 10:56:51 +0300

    V332: display version consistently as 3.32 (#34)
    * V332: display release number as 3.32: app-03.js
    
    * V332: display release number as 3.32: app-08.js
    
    * V332: display release number as 3.32: index.html

--- app-08.js
+++ app-08.js
@@ -234,7 +234,7 @@ document.addEventListener('input',e=>{
 
 function V173SyncArchitectureAudit(){
  return {
-  version:window.APP_BUILD_VERSION||'V331',
+  version:window.APP_BUILD_VERSION||'3.32',
   realtimePrimary:false,
   startupFullPull:false,
   periodicFallbackMs:30000,
